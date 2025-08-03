18d5db20b25ff3b628b7cd06e0166b611f874aa8
Michael Ball <aaronhunter@live.com>
2025-08-02 22:20:26 -0500

    alpha constants

--- src/main/java/clock/util/Constants.java
+++ src/main/java/clock/util/Constants.java
@@ -10,103 +10,105 @@ public class Constants {
 
     private Constants() {}
 
-    public static final String HIDE = "Hide";
-    public static final String SHOW = "Show";
-    public static final String SPACE = " ";
-    public static final String EMPTY = "";
-    public static final String DIGITAL_TIME = "digital time";
-    public static final String STANDARD_TIME_SETTING = "standard time";
-    public static final String MILITARY_TIME_SETTING = "military time";
-    public static final String FULL_TIME_SETTING = "full date";
-    public static final String PARTIAL_TIME_SETTING = "partial date";
-    public static final String DST_SETTING = "daylight savings time";
-    public static final String Turn = "Turn";
-    public static final String on = "on";
-    public static final String off = "off";
-    public static final String STAR = "*";
-    public static final String SLASH = "/";
+    // General
+    public static final String AM = "AM";
+    public static final String are = "are";
     public static final String COLON = ":";
     public static final String COMMA = ",";
-    public static final String NEWLINE = "\n";
-    public static final String NO = "No";
-    public static final String NAME = "Name";
-    public static final String Hours = "Hours";
-    public static final String Minutes = "Minutes";
-    public static final String Seconds = "Seconds";
-    public static final String LABEL = "Label";
-    public static final String FIELD = "Field";
-    public static final String ONE = "1";
-    public static final String TWO = "2";
-    public static final String THREE = "3";
-    public static final String FOUR = "4";
-    public static final String FIVE = "5";
-    public static final String SIX = "6";
-    public static final String SEVEN = "7";
+    public static final String CLOCK = "Clock";
+    public static final String DIGITAL_TIME = "digital time";
+    public static final String DST_SETTING = "daylight savings time";
     public static final String EIGHT = "8";
-    public static final String NINE = "9";
-    public static final String TEN = "10";
     public static final String ELEVEN = "11";
-    public static final String TWELVE = "12";
-    public static final String AM = "AM";
-    public static final String PM = "PM";
-    public static final String S = "S";
+    public static final String EMPTY = "";
+    public static final String FIELD = "Field";
+    public static final String FIVE = "5";
+    public static final String FOUR = "4";
+    public static final String FULL_TIME_SETTING = "full date";
+    public static final String going_off = "going off!";
+    public static final String HIDE = "Hide";
+    public static final String Hours = "Hours";
+    public static final String is = "is";
+    public static final String LABEL = "Label";
+    public static final String MIDNIGHT_MILITARY_TIME = "2400 hours 00"; // check on 0000 hours 00
+    public static final String MIDNIGHT_STANDARD_TIME = "12:00:00 AM";
+    public static final String MILITARY_TIME_SETTING = "military time";
+    public static final String Minutes = "Minutes";
+    public static final String NAME = "Name";
+    public static final String NEWLINE = "\n";
+    public static final String NINE = "9";
+    public static final String NO = "No";
     public static final String No_Alarms = "No Alarms";
     public static final String No_Timers = "No Timers";
-    public static final String is = "is";
-    public static final String are = "are";
-    public static final String going_off = "going off!";
-    public static final String MIDNIGHT_STANDARD_TIME = "12:00:00 AM";
-    public static final String MIDNIGHT_MILITARY_TIME = "2400 hours 00"; // check on 0000 hours 00
+    public static final String off = "off";
+    public static final String on = "on";
+    public static final String ONE = "1";
+    public static final String PARTIAL_TIME_SETTING = "partial date";
+    public static final String PM = "PM";
+    public static final String S = "S";
+    public static final String Seconds = "Seconds";
+    public static final String SEVEN = "7";
+    public static final String SIX = "6";
+    public static final String SHOW = "Show";
+    public static final String SPACE = " ";
+    public static final String STANDARD_TIME_SETTING = "standard time";
+    public static final String STAR = "*";
+    public static final String SLASH = "/";
+    public static final String TEN = "10";
+    public static final String THREE = "3";
+    public static final String Turn = "Turn";
+    public static final String TWELVE = "12";
+    public static final String TWO = "2";
 
     // Time zones
-    public static final String TIME_ZONES = "time zones";
-    public static final String SETTINGS = "Settings";
     public static final String FEATURES = "Features";
+    public static final String SETTINGS = "Settings";
+    public static final String TIME_ZONES = "time zones";
 
-    public static final String HAWAII = "Hawaii";
     public static final String ALASKA = "Alaska";
-    public static final String PACIFIC = "Pacific";
     public static final String CENTRAL = "Central";
     public static final String EASTERN = "Eastern";
+    public static final String HAWAII = "Hawaii";
+    public static final String PACIFIC = "Pacific";
 
     // ZoneIds
-    public static final String PACIFIC_HONOLULU = "Pacific/Honolulu";
     public static final String AMERICA_ANCHORAGE = "America/Anchorage";
-    public static final String AMERICA_LOS_ANGELES = "America/Los_Angeles";
     public static final String AMERICA_CHICAGO = "America/Chicago";
+    public static final String AMERICA_LOS_ANGELES = "America/Los_Angeles";
     public static final String AMERICA_NEW_YORK = "America/New_York";
+    public static final String PACIFIC_HONOLULU = "Pacific/Honolulu";
+
     //String
 
     // Timer
-    public static final String SET = "Set";
-    public static final String TIMER = "Timer";
-    public static final String VIEW_TIMER = "View Timer";
-    public static final String VIEW_STOPWATCH = "View Stopwatch";
-    public static final String RESUME_TIMER = "Resume Timer";
-    public static final String PAUSE_TIMER = "Pause Timer";
-    public static final String RESET = "Reset";
-    public static final String STOP = "Stop";
     public static final String COMPLETE = "Complete";
+    public static final String CURRENT_TIMERS = "Current Timers";
     public static final String HOUR = "Hour";
     public static final String MIN = "Min";
+    public static final String PAUSE_TIMER = "Pause Timer";
+    public static final String REMOVE = "Remove";
+    public static final String RESET = "Reset";
+    public static final String RESUME_TIMER = "Resume Timer";
     public static final String SEC = "Sec";
-    public static final String ZERO = "0";
-    public static final String TIMER_HOUR_ERROR_24 = "0 <= "+HOUR+" > 24";
+    public static final String SET = "Set";
+    public static final String TIMER = "Timer";
     public static final String TIMER_HOUR_ERROR_12 = "0 <= "+HOUR+" > 12";
+    public static final String TIMER_HOUR_ERROR_24 = "0 <= "+HOUR+" > 24";
     public static final String TIMER_MIN_ERROR = "0 <= "+MIN+" > 60";
     public static final String TIMER_SEC_ERROR = "0 <= "+SEC+" > 60";
-    public static final String CURRENT_TIMERS = "Current Timers";
+    public static final String VIEW_TIMER = "View Timer";
+    public static final String VIEW_STOPWATCH = "View Stopwatch";
+    public static final String ZERO = "0";
 
     // Alarm
-    public static final String SET_ALARMS = "Set Alarms";
-    public static final String VIEW_ALARMS = "View Alarms";
-    public static final String ALARM = "Alarm";
     public static final String ADDED = "Added";
+    public static final String ALARM = "Alarm";
     public static final String AMPM = "AM/PM";
     public static final String CURRENT_ALARMS = "Current Alarms";
+    public static final String SET_ALARMS = "Set Alarms";
+    public static final String VIEW_ALARMS = "View Alarms";
     public static final String WEEK = "WK";
     public static final String WEEKEND = "WKD";
-
     public static final String VIEW_DIGITAL_CLOCK = "View Digital Clock";
     public static final String VIEW_ANALOGUE_CLOCK = "View Analogue Clock";
 }
